3785464fe8765dda7aaa80ce924b5234e4dd924e
Gswaraw37 <genta.swara@gmail.com>
2025-06-17 16:53:03 +0700

    first commit

---
+++ src/constants/facialExpressions.js
@@ -0,0 +1,25 @@
+const facialExpressions = {
+  default: {},
+  smile: {
+    browInnerUp: 0.17,
+    eyeSquintLeft: 0.4,
+    eyeSquintRight: 0.44,
+    noseSneerLeft: 0.1700000727403593,
+    noseSneerRight: 0.14000002836874015,
+    mouthPressLeft: 0.61,
+    mouthPressRight: 0.41000000000000003,
+  },
+  sad: {
+    mouthFrownLeft: 1,
+    mouthFrownRight: 1,
+    mouthShrugLower: 0.78341,
+    browInnerUp: 0.452,
+    eyeSquintLeft: 0.72,
+    eyeSquintRight: 0.75,
+    eyeLookDownLeft: 0.5,
+    eyeLookDownRight: 0.5,
+    jawForward: 1,
+  },
+};
+
+export default facialExpressions;
